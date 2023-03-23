3e5afdbbbf221b42771dc6f484e07c729660e95e
Fajar Ghifari <fajarghifari3@gmail.com>
2023-03-23 12:34:00 +0700

    build : update the password in the Edit Profile view

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,6 +1,42 @@
 @extends('dashboard.body.main')
 
 @section('container')
+    @if (session('status') === 'password-updated')
+    <!--begin::Alert-->
+    <div class="alert alert-dismissible bg-success d-flex flex-column flex-sm-row w-100 p-5 mb-10">
+        <!--begin::Icon-->
+            <!--begin::Svg Icon | path: icons/duotune/art/art005.svg-->
+            <span class="svg-icon svg-icon-2hx svg-icon-light me-4 mb-5 mb-sm-0">
+                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path opacity="0.3" d="M21.4 8.35303L19.241 10.511L13.485 4.755L15.643 2.59595C16.0248 2.21423 16.5426 1.99988 17.0825 1.99988C17.6224 1.99988 18.1402 2.21423 18.522 2.59595L21.4 5.474C21.7817 5.85581 21.9962 6.37355 21.9962 6.91345C21.9962 7.45335 21.7817 7.97122 21.4 8.35303ZM3.68699 21.932L9.88699 19.865L4.13099 14.109L2.06399 20.309C1.98815 20.5354 1.97703 20.7787 2.03189 21.0111C2.08674 21.2436 2.2054 21.4561 2.37449 21.6248C2.54359 21.7934 2.75641 21.9115 2.989 21.9658C3.22158 22.0201 3.4647 22.0084 3.69099 21.932H3.68699Z" fill="currentColor"></path>
+                    <path d="M5.574 21.3L3.692 21.928C3.46591 22.0032 3.22334 22.0141 2.99144 21.9594C2.75954 21.9046 2.54744 21.7864 2.3789 21.6179C2.21036 21.4495 2.09202 21.2375 2.03711 21.0056C1.9822 20.7737 1.99289 20.5312 2.06799 20.3051L2.696 18.422L5.574 21.3ZM4.13499 14.105L9.891 19.861L19.245 10.507L13.489 4.75098L4.13499 14.105Z" fill="currentColor"></path></svg>
+            </span>
+            <!--end::Svg Icon-->
+        <!--end::Icon-->
+
+        <!--begin::Content-->
+        <div class="d-flex flex-column text-light pe-0 pe-sm-10">
+            <h4 class="mb-2 text-light">Success!</h4>
+            <span>Password has been updated.</span>
+        </div>
+        <!--end::Content-->
+
+        <!--begin::Close-->
+        <button type="button" class="position-absolute position-sm-relative m-2 m-sm-0 top-0 end-0 btn btn-icon ms-sm-auto" data-bs-dismiss="alert">
+            <!--begin::Svg Icon | path: icons/duotune/arrows/arr061.svg-->
+            <span class="svg-icon svg-icon-2x svg-icon-light">
+                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <rect opacity="0.5" x="6" y="17.3137" width="16" height="2" rx="1" transform="rotate(-45 6 17.3137)" fill="currentColor"></rect>
+                    <rect x="7.41422" y="6" width="16" height="2" rx="1" transform="rotate(45 7.41422 6)" fill="currentColor"></rect>
+                </svg>
+            </span>
+            <!--end::Svg Icon-->
+        </button>
+    <!--end::Close-->
+    </div>
+    <!--end::Alert-->
+    @endif
+
     <!--begin::Navbar-->
     @include('profile.partials.navbar-profile-information')
     <!--end::Navbar-->
--- resources/views/profile/partials/update-password-form.blade.php
+++ resources/views/profile/partials/update-password-form.blade.php
@@ -1,48 +1,70 @@
-<section>
-    <header>
-        <h2 class="text-lg font-medium text-gray-900">
-            {{ __('Update Password') }}
-        </h2>
-
-        <p class="mt-1 text-sm text-gray-600">
-            {{ __('Ensure your account is using a long, random password to stay secure.') }}
-        </p>
-    </header>
-
-    <form method="post" action="{{ route('password.update') }}" class="mt-6 space-y-6">
-        @csrf
-        @method('put')
-
-        <div>
-            <x-input-label for="current_password" :value="__('Current Password')" />
-            <x-text-input id="current_password" name="current_password" type="password" class="mt-1 block w-full" autocomplete="current-password" />
-            <x-input-error :messages="$errors->updatePassword->get('current_password')" class="mt-2" />
+<div class="card mb-5 mb-xl-10">
+    <!--begin::Card header-->
+    <div class="card-header border-0">
+        <div class="card-title m-0">
+            <h3 class="fw-bold m-0">Change Password</h3>
         </div>
+    </div>
+    <!--end::Card header-->
 
-        <div>
-            <x-input-label for="password" :value="__('New Password')" />
-            <x-text-input id="password" name="password" type="password" class="mt-1 block w-full" autocomplete="new-password" />
-            <x-input-error :messages="$errors->updatePassword->get('password')" class="mt-2" />
+    <!--begin::Card body-->
+    <div class="card-body border-top p-9">
+        <!--begin::Password-->
+        <div class="d-flex flex-wrap align-items-center mb-10">
+            <!--begin::Edit-->
+            <div class="flex-row-fluid">
+                <!--begin::Form-->
+                <form action="{{ route('password.update') }}" method="POST" class="form">
+                    @csrf
+                    @method('put')
+
+                    <div class="row mb-1">
+                        <div class="col-lg-4">
+                            <div class="fv-row mb-0">
+                                <label for="current_password" class="form-label fs-6 fw-bold mb-3">Current Password</label>
+                                <input type="password" class="form-control form-control-lg form-control-solid @error('current_password') is-invalid @enderror" name="current_password" id="current_password" required>
+                                <div class="invalid-feedback">
+                                    @error('current_password')
+                                    {{ $message }}
+                                    @enderror
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-4">
+                            <div class="fv-row mb-0">
+                                <label for="password" class="form-label fs-6 fw-bold mb-3">New Password</label>
+                                <input type="password" class="form-control form-control-lg form-control-solid @error('password') is-invalid @enderror" name="password" id="password" required>
+                                <div class="invalid-feedback">
+                                    @error('password')
+                                    {{ $message }}
+                                    @enderror
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-4">
+                            <div class="fv-row mb-0">
+                                <label for="password_confirmation" class="form-label fs-6 fw-bold mb-3">Confirm New Password</label>
+                                <input type="password" class="form-control form-control-lg form-control-solid @error('password_confirmation') is-invalid @enderror" name="password_confirmation" id="password_confirmation" required>
+                                <div class="invalid-feedback">
+                                    @error('password_confirmation')
+                                    {{ $message }}
+                                    @enderror
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="d-flex">
+                        <button type="submit" class="btn btn-primary me-2 px-6">Update Password</button>
+                        <a href="{{ route('profile') }}" class="btn btn-color-gray-400 btn-active-light-primary px-6">Cancel</a>
+                    </div>
+                </form>
+                <!--end::Form-->
+            </div>
+            <!--end::Edit-->
         </div>
-
-        <div>
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-            <x-text-input id="password_confirmation" name="password_confirmation" type="password" class="mt-1 block w-full" autocomplete="new-password" />
-            <x-input-error :messages="$errors->updatePassword->get('password_confirmation')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center gap-4">
-            <x-primary-button>{{ __('Save') }}</x-primary-button>
-
-            @if (session('status') === 'password-updated')
-                <p
-                    x-data="{ show: true }"
-                    x-show="show"
-                    x-transition
-                    x-init="setTimeout(() => show = false, 2000)"
-                    class="text-sm text-gray-600"
-                >{{ __('Saved.') }}</p>
-            @endif
-        </div>
-    </form>
-</section>
+        <!--end::Password-->
+    </div>
+    <!--end::Card body-->
+</div>
